baf21676def6bc00a3a62aeb388244de0b08973b
RRP12 <rushikeshpatil8208@gmail.com>
2024-12-11 23:13:55 +0530

    build fix

--- app/auth-button-clinet.js
+++ app/auth-button-clinet.js
@@ -13,7 +13,7 @@ export default function AuthButtonClinet({ session }) {
     await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: `${location.origin}/auth/callback`,
       },
     })
   }
